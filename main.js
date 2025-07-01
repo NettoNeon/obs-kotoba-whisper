794da7e44598974b3230a855ead22478e0cc87a3
NettoNeon_legion <136179995+NettoNeon@users.noreply.github.com>
2025-07-02 02:04:39 +0900

    electron追加 起動テスト

---
+++ main.js
@@ -0,0 +1,24 @@
+import { app, BrowserWindow } from "electron/main";
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // for macOS
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// app exit
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
