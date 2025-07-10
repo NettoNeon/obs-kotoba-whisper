3f792dbe8f8e7cc2468568b2b410e4244b717336
NettoNeon <136179995+NettoNeon@users.noreply.github.com>
2025-07-11 08:17:37 +0900

    TS追加

--- forge.config.js
+++ forge.config.js
@@ -33,12 +33,12 @@ module.exports = {
         build: [
           {
             // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-            entry: "src/backend/main.js",
+            entry: "src/backend/main.ts",
             config: "vite.main.config.mjs",
             target: "main",
           },
           {
-            entry: "src/backend/preload.js",
+            entry: "src/backend/preload.ts",
             config: "vite.preload.config.mjs",
             target: "preload",
           },
--- src/components/App.vue
+++ src/components/App.vue
@@ -3,6 +3,6 @@
   <p>Welcome to your Electron application.</p>
 </template>
 
-<script setup>
+<script setup lang="ts">
 console.log('👋 This message is being logged by "App.vue", included via Vite');
 </script>
