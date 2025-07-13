f36176adf0d4c23f2a427091535a05189f4b9402
NettoNeon_legion <136179995+NettoNeon@users.noreply.github.com>
2025-07-13 23:56:48 +0900

    wip

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <h1>💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <section>
+    <h1>文字起こし</h1>
+    <div>
+      <button id="start-rec" class="btn">開始</button>
+      <button id="stop-rec" class="btn">停止</button>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
