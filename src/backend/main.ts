49b8e91f256c5667923b9e55ccaeadbbc4aa5582
NettoNeon_legion <136179995+NettoNeon@users.noreply.github.com>
2025-07-13 21:41:07 +0900

    cssとCSPの設定

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -28,24 +28,20 @@ const createWindow = () => {
   mainWindow.webContents.openDevTools();
 
   // https://www.electronjs.org/docs/latest/tutorial/security#7-define-a-content-security-policy
-  // session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-  //   callback({
-  //     responseHeaders: {
-  //       ...details.responseHeaders,
-  //       "Content-Security-Policy": ["default-src 'none'"],
-  //     },
-  //   });
-  // });
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        "Content-Security-Policy": "default-src 'self'; style-src 'self'",
+      },
+    });
+  });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+  // for mac
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -53,14 +49,9 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// for mac
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
--- src/components/renderer.ts
+++ src/components/renderer.ts
@@ -1,6 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "../scss/reset.scss";
-import "../scss/style.scss";
 
 createApp(App).mount("#app");
