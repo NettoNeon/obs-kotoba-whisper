347d566bed8c6c08b1506cd168450e77f329c5e6
NettoNeon_legion <136179995+NettoNeon@users.noreply.github.com>
2025-07-13 22:05:04 +0900

    storybookの調整

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,22 @@
-import type { StorybookConfig } from '@storybook/vue3-vite';
+import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
-  ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {}
-  }
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx|vue)"],
+  addons: ["@chromatic-com/storybook", "@storybook/addon-docs", "@storybook/addon-onboarding", "@storybook/addon-a11y", "@storybook/addon-vitest"],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  viteFinal: async (config, { configType }) => {
+    config.plugins = config.plugins || [];
+    config.plugins.push(require("@vitejs/plugin-vue").default());
+
+    // 以下の設定は、もしVue3 Composition APIのSetup Scriptを使用している場合に必要になることがあります
+    // config.optimizeDeps = config.optimizeDeps || {};
+    // config.optimizeDeps.include = config.optimizeDeps.include || [];
+    // config.optimizeDeps.include.push('vue');
+
+    return config;
+  },
 };
 export default config;
